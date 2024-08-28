d077484a04538a873d2567d0265689efd1396b46
DhanyataRangaswamy <rangaswamydhanyata@gmail.com>
2024-08-28 18:16:01 +0530

    First commit

---
+++ try.py
@@ -0,0 +1,11 @@
+valid = False
+
+while not valid:
+  try:
+   num=int(input("Enter a number: "))
+   while num%2==0:
+    print("Bye")
+   valid=True 
+   
+  except ValueError:
+   print("The input is invalid")
