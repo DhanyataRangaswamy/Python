ef0f683bb033dd90ab07346c25d17be5a98d1bfc
DhanyataRangaswamy <rangaswamydhanyata@gmail.com>
2024-09-11 18:07:33 +0530

    countrycode

---
+++ L3 Dictionary/countrycode.py
@@ -0,0 +1,7 @@
+ctry_code={'India':'0091',
+           'Nepal':'0977',
+           'Australia':'0025'}
+print("The county code for india ")
+print(ctry_code.get('India','Not found'))
+print("The county code for japan ")
+print(ctry_code.get('Japan','Not found'))
