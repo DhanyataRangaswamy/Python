ce05adc2578b173008019a43d745af4ed6386917
DhanyataRangaswamy <rangaswamydhanyata@gmail.com>
2024-09-15 12:23:24 +0530

    sets 1

---
+++ L4 Sets and array/sets.py
@@ -0,0 +1,14 @@
+my_set={1,2,3,4,5}
+print(my_set)
+my_set1={1,0.02,"hello",45}
+print(my_set1)
+my_set2={1,2,3,1,2,4,1}
+print(my_set2)
+myset3=set([1,2,3,4])
+print(myset3)
+num_set=set([1,2,3,4,5])
+print("The original set: ")
+print(num_set)
+num_set.pop()
+print("After removing the last element: ")
+print(num_set)
