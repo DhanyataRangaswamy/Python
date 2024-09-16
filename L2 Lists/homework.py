45d87429dcbc23712bb05caa1547a08679341619
DhanyataRangaswamy <rangaswamydhanyata@gmail.com>
2024-09-16 22:44:08 +0530

    homeworks lists

---
+++ L2 Lists/homework.py
@@ -0,0 +1,12 @@
+
+num=[1,2,3,4,5,6,7,8,]
+sq=1
+
+for i in num:
+    sq=i**2
+    print("The sq of the number is: ", sq)
+    if sq%2==0:
+        print("the above square is even")
+    else:
+        print("the above square is odd")
+
