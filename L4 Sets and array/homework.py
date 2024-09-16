2a5b14945a082d9f81ff5b946d4f4554738e1f5b
DhanyataRangaswamy <rangaswamydhanyata@gmail.com>
2024-09-16 23:07:46 +0530

    homework sd

---
+++ L4 Sets and array/homework.py
@@ -0,0 +1,8 @@
+setx={"4","5","6"}
+sety={"1","2","3"}
+print("The original sets are: ")
+print(setx)
+print(sety)
+setz=setx.symmetric_difference(sety)
+print("Their symmetric difference is: ")
+print(setz)
