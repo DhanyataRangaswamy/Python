38bf4b06f48272a88cbfe4480c994b9fcc91e942
DhanyataRangaswamy <rangaswamydhanyata@gmail.com>
2024-10-16 18:25:40 +0530

    multiplying tuples

--- L3 tuple/homework.py
+++ L3 tuple/homework.py
@@ -1,6 +1,14 @@
 tup1=(4,3,2,2,-1,18)
 tup2=(2,4,8,8,3,2,9)
-
+pro=1
+pro2=1
+print("The original tuples are: \n",tup1,"\n",tup2)
 for i in tup1:
-    i*tup2
+    pro=pro*i
+print ("The product of tuple 1 is: ",pro)
+
+for j in tup2:
+  pro2=pro2*j
+print("The product of tuple 2 is: ",pro2)
+    
     
